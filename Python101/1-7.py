775b1370a9cd635781dfee28e10d78c1148b4b25
Jo Taijiquan <taijimaster@gmail.com>
2018-06-15 03:33:51 +0700

    update function

--- Python101/1-7.py
+++ Python101/1-7.py
@@ -2,7 +2,7 @@
 
 #Function
 
-
+'''
 #Example 7.00
 def example_700():
     print ("x inside function",x)
@@ -10,28 +10,16 @@ def example_700():
 x=10
 example_700()
 print ("x outside function",x)
-
 '''
-#Error
+
+
+#Example 7.01
 def example_701():
-    print (x)
-    x=20
-
-x=10
-example_701()
-print(x)
-'''
-
-'''
-#Example 7.02
-def example_702():
     x = 20
     print (x)
-    x = x+10
 
-x=10
+x = 10
 print (x)
-example_702()
+example_701()
 print (x)
-'''
 
