90cb8757658de65374701fbf5669a61d729dd434
Jo Taijiquan <taijimaster@gmail.com>
2018-06-14 20:43:38 +0700

    update list

--- Python101/1-6.py
+++ Python101/1-6.py
@@ -45,6 +45,64 @@ def example_605():
             x.extend([10,11,12,13])
     print (x)
 
-example_605()
+#example_605()
 
+def example_606():
+    for i in "Hello World!":
+        print (i)
+#example_606()
 
+def example_607():
+    for i in str(3.14159):
+        print (i)
+#example_607()
+
+def example_608():
+    for i in ("Superman", "Batman", "Aquaman"):
+        print ("Hello",i)
+#example_608()
+
+def example_609():
+    i=8
+    for i in 3,5,7,9,"Superman",False,[1,2,3],(3,4,5),[],(),None,i:
+        print (i)
+#example_609()
+
+def example_610():
+    for i in range(10):
+        if i==5:
+            pass
+        else:
+            print (i)
+#example_610()
+
+def example_611():
+    for i in range(10):
+        if i==4:
+            continue
+        else:
+            print (i)
+#example_611()
+
+def example_612():
+    for i in range(10):
+        if i==5:
+            break
+        else:
+            print (i)
+
+#example_612()
+
+def example_613():
+    x = [1,2,3]
+    try:
+        print (x[2])
+    except:
+        print ("Ha Ha Ha")
+        
+    try:
+        print (x[6])
+    except:
+        print ("Error x is out of range")
+        
+example_613()
