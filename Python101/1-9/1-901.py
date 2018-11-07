888391d8c3e6c3f021e3f793a525c8198fece1f3
Jo <jo@Jos-MacBook-Pro.local>
2018-11-08 01:43:52 +0700

    python101-Revised Update

--- Python101/1-9/1-901.py
+++ Python101/1-9/1-901.py
@@ -10,7 +10,20 @@ tom.say()
 
 
 '''
-Class พูดกันยาวละ
+class เป็นการเขียน program แบบ object oriented programming
+ภาคทฤษฎีค่อยว่ากัน ดูตัวอย่างก่อน
+
+class Cat:
+    def say(self):
+        print ("Meow")
+
+เป็นการสร้าง class ชื่อ Cat ซึ่งมีสมาชิกเป็น function ชื่อ say(self)
+
+tom = Cat() เป็นการกำหนดให้ tom เป็นตัวแทน class Cat
+ซึ่ง tom ก็จะใช้ความสามารถทุกอย่างของ class Cat ได้ ซึ่งตอนนี้มีอย่าสงเดียวคือ function say(self)
+self ใน function say(self) เอาไว้อ้างถึงตัวเอง ยังไม่ต้องสนใจ
+
+tom.say() เป็นการเรียกใช้ function ของ class Cat
 
 แสดงผล
 Meow
