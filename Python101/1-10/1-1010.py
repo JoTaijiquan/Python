08f1fdc1178b3c1ad6954f0b6ed39549a9d4250c
JoTaijiquan <taijimaster@gmail.com>
2018-12-03 14:03:43 +0700

    a little update

---
+++ Python101/1-10/1-1010.py
@@ -0,0 +1,29 @@
+#Example 10.10
+#Python 3.7.1
+
+import random
+import turtle
+
+
+t=turtle
+r=random
+
+r.seed()
+
+t.setup(800,600)
+t.title("Random Line")
+t.speed(500)
+t.colormode(255)
+
+for i in range(100):
+    rx=r.randint(-400,400)
+    ry=r.randint(-290,290)
+    c=r.randint(0,255),r.randint(0,255),r.randint(0,255)
+    t.pencolor(c)
+    t.goto(rx,ry)
+    t.pensize(i%10)
+
+'''
+
+แสดงผล
+'''
