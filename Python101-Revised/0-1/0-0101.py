a51878e322ec19f024d008ccb23e1eb237ebeae9
JoTaijiquan <taijimaster@gmail.com>
2019-03-21 18:27:20 +0700

    update

---
+++ Python101-Revised/0-1/0-0101.py
@@ -0,0 +1,6 @@
+#Example 0.1.1
+#Python 3.6.5
+#Created By Jooompot Sriyapan
+
+print ("Hello World!")
+print ("สวัสดีชาวโลก!")
---
+++ Python101-Revised/0-1/0-0102.py
@@ -0,0 +1,9 @@
+#Example 0.1.1
+#Python 3.6.5
+#Created By Jooompot Sriyapan
+
+print ("มาเขียนโปรแกรมภาษา Python ด้วยความสนุกสนานกันเถิด")
+print (55555)
+print (555+444)
+print (555,666,777)
+print ("ห้าห้าห้า",666,333*3,6.5/3.4)
