605c6e2971f14c7fdd1b76be1276ed951cb04a26
Jo Taijiquan <taijimaster@gmail.com>
2019-08-18 19:16:11 +0700

    update

--- Python101-New130519/2-8-1.py
+++ Python101-New130519/2-8-1.py
@@ -7,17 +7,10 @@ def square_root(n):
     n1 = n
     for i in range(100):
         n_mid = (n0+n1)/2
-<<<<<<< HEAD:Python101-New130519/2-8-1.py
         n2 = n_mid*n_mid
         if n2 == n:
             return n_mid
         elif n2 < n:
-=======
-        ans = n_mid*n_mid
-        if ans == n:
-            return n_mid
-        elif ans < n:
->>>>>>> a93837d799dd3ecaa382d2fef890ac431dae761d:Python101-New130519/New folder/2-11-1.py
             n0 = n_mid
         elif n2 > n:
             n1 = n_mid
