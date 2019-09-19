aa184ec92eb1e294fa3ee440cf0e74fe49e99a91
Jo Taijiquan <taijimaster@gmail.com>
2019-09-20 02:46:56 +0700

    invader update

--- Pygame/invader3.py
+++ Pygame/invader3.py
@@ -1,6 +1,6 @@
 #Python 3.7.4
 #Pygame 1.9.6
-#Example invader2
+#Example invader3
 
 import pygame, sys,random
 from pygame.locals import *
@@ -56,10 +56,41 @@ class Invader(pygame.sprite.Sprite):
         super(Invader,self).__init__()
         self.image, self.rect = load_image(IMG_NAME['invader1'],-1)
         self.rect.center = (x,y)
+        self.direction = "RIGHT"
+        self.move_x = 0
+        self.move_y = 0
+
+    def move(self):
+        if self.direction == "RIGHT":
+            self.rect.move_ip(3,0)
+            self.move_x+=1
+            if self.move_x == 20:
+                self.move_x = 0
+                self.direction = "RIGHTDOWN"
+        elif self.direction == "RIGHTDOWN":
+            self.rect.move_ip(0,3)
+            self.move_y+=1
+            if self.move_y ==3:
+                self.move_y = 0
+                self.direction = "LEFT"
+        elif self.direction == "LEFT":
+            self.rect.move_ip(-3,0)
+            self.move_x+=1
+            if self.move_x ==20:
+                self.move_x=0
+                self.direction = "LEFTDOWN"
+        elif self.direction == "LEFTDOWN":
+            self.rect.move_ip(0,3)
+            self.move_y+=1
+            if self.move_y ==3:
+                self.move_y = 0
+                self.direction = "RIGHT"
     
+    def update(self):
+        self.move()
+
     def __del__(self):
         pass
-        #self.rect.center = (0,0)
 
 class App:
     def __init__(self):
@@ -90,11 +121,11 @@ def mainloop(app):
     app.planes.add(player1)
     bullet = ""
 
-    invader1 = Invader(100,40)
-    app.invaders.add(invader1)
-    invader1 = Invader(200,40)
-    app.invaders.add(invader1)
-
+    for j in range (5):
+        for i in range (8):
+            invader1 = Invader(80*i+50,j*70+50)
+            app.invaders.add(invader1)
+ 
     while True:    
         key = pygame.key.get_pressed() 
         if key[pygame.K_LEFT]:
@@ -108,11 +139,10 @@ def mainloop(app):
             else:
                 delay = bullet.delay
             
-            if delay>5:
+            if delay>3:
                 bullet = Bullet(player1.rect.x, player1.rect.y)
                 bullet.delay = 0
                 app.bullets.add(bullet)
-                #app.allsprites.add(bullet)
             
         for event in pygame.event.get():
             if event.type == QUIT:
